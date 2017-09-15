76807bf1b14c55a88e7773bbc37158647387d9fa
lucasfg67 <lucasfg67@hotmail.com>
2017-09-15 19:42:35 -0300

    Diretorio de scripts

---
+++ SQL/Tabelas.sql
@@ -0,0 +1,194 @@
+﻿create table equipamento(
+	id  	integer      not null,
+	nome    varchar(100) not null,
+	marca   varchar(100) not null,
+	n_serie integer      not null,	
+	
+	constraint pk_equipamento primary key (id)	
+);
+drop table equipamento;
+create generator gen_equipamento_id;
+	
+create table tecnico(
+	id     		integer     not null,
+	nome       	varchar(60) not null,
+	valor_hora 	float       not null,
+	
+	constraint pk_tecnico primary key (id)	
+);
+
+create generator gen_tecnico_id;
+
+create table material(
+	id         		integer      not null,
+	descricao      	varchar(100) not null,
+	valor_unitario 	float        not null,
+	
+	constraint pk_material primary key (id)
+);
+
+create generator gen_material_id;
+
+create table cidade(
+	id 		  integer     not null,
+	nome 	  varchar(60) not null,
+	id_estado integer     not null,
+	
+	constraint pk_cidade primary key (id),
+	
+	constraint fk_cidade_estado foreign key 
+		(id_estado) references estado (id);
+);
+
+
+create generator gen_cidade_id;
+
+alter table cidade 
+add id_estado integer not null,
+add constraint fk_cidade_estado foreign key 
+	(id_estado) references estado (id);
+
+create table cliente(
+	id        		integer      not null,
+	nome		  	varchar(100) not null,
+	solicitante   	varchar(60)  not null,
+	cnpj_cpf      	varchar(60)  not null,
+	telefone      	varchar(60)  not null,
+	id_cidade 		integer      not null,
+	endereco      	varchar(100) not null,
+	tipo_pessoa     varchar(1)   not null,
+	constraint pk_cliente primary key (id),
+	
+	constraint fk_cliente_cidade foreign key 
+		(id_cidade) references cidade(id)
+);
+
+create generator gen_cliente_id;
+
+
+create table os(
+	id                	integer       not null,
+	data_entrada 		date          not null,
+	descricao_servico 	varchar(4000) not null,
+	id_cliente        	integer       not null,
+	id_equipamento 	  	integer       not null,	
+	
+	constraint pk_os primary key (id),
+	
+	constraint fk_os_cliente foreign key 
+		(id_cliente) references cliente(id),
+		
+	constraint fk_os_equipamento foreign key 
+		(id_equipamento)  references equipamento(id)
+	
+);
+
+create generator gen_os_id;
+
+create table os_tecnico(
+	id         		integer   not null,
+	id_tecnico 		integer   not null,
+	id_os      		integer   not null,
+	data           	date      not null,
+	inicio_manha   	timestamp not null, 
+	termino_manha  	timestamp not null, 
+	inicio_tarde   	timestamp not null,
+	termino_tarde  	timestamp not null,
+	
+	constraint pk_os_tecnico primary key (id),
+	
+	constraint fk_os_tecnico_tecnico foreign key 
+		(id_tecnico) references tecnico(id),
+		
+	constraint fk_os_tecnico_os foreign key 
+		(id_os)      references os(id)
+);
+
+create generator gen_os_tecnico_id;
+
+create table os_material(
+	id          	integer not null,
+	id_os       	integer not null,
+	id_material 	integer not null,
+	quantidade 	    integer not null,
+		
+	constraint pk_os_material primary key (id),
+	
+	constraint fk_os_material_material foreign key 
+		(id_material) references material(id),
+		
+	constraint fk_os_material foreign key 
+		(id_os)      references os(id)
+);
+
+create generator gen_os_material_id;
+
+create table usuario (
+	id    integer     not null,
+	nome  varchar(40) not null,
+	senha varchar(40) not null,
+	
+	constraint pk_usuario primary key (id)
+);
+
+create generator gen_usuario_id;
+
+create table movimentacao (
+	id          integer    not null,
+	operacao    varchar(1) not null,
+	data        timestamp  not null,
+	id_material integer    not null,
+	quantidade  float      not null,
+	situacao    varchar(1) not null,
+	
+	constraint pk_movimentacao primary key (id),
+	
+	constraint fk_movimentacao_material foreign key 
+		(id_material) references material(id)
+);
+
+create generator gen_movimentacao;
+
+create table os_mat_movi(
+	id              integer not null,
+	id_os_material  integer not null,
+	id_movimentacao integer not null,
+	
+	constraint pk_os_mat_movi primary key (id),
+	
+	constraint fk_os_mat_movi_os_material foreign key 
+		(id_os_material) references os_material(id),
+		
+	constraint fk_os_mat_movi_movimentcao foreign key 
+		(id_movimentacao) references movimentacao(id)
+
+);
+
+create generator gen_os_mat_movi;
+
+
+create table pais (
+	id    integer     not null,
+	nome  varchar(60) not null,
+	sigla varchar(5)  not null,
+	
+	
+	constraint pk_pais primary key (id)
+);
+
+create generator gen_pais;
+
+create table estado (
+	id      integer      not null,
+	nome    varchar (60) not null,
+	uf      varchar(10)  not null,
+	id_pais integer      not null,
+	
+	constraint pk_estado primary key (id),
+	
+	constraint fk_estado_pais foreign key 
+	 	(id_pais) references pais(id)
+
+);
+
+create generator gen_estado;
---
+++ SQL/Views.sql
@@ -0,0 +1,74 @@
+﻿create view vw_cidade as
+select c.id           as "Cód."
+	 , c.nome 		  as "Cidade"
+	 , e.nome	 	  as "Estado"
+  from cidade c inner join estado e on (e.id = c.id_estado);
+  
+ create view vw_cliente as
+ select cl.id           as "Cód."
+       ,cl.nome         as "Nome"
+       ,cl.solicitante  as "Solicitante"
+       ,cl.cnpj_cpf     as "CNPJ/CPF"
+       ,cl.telefone     as "Telefone"
+       ,c.nome		    as "Cidade"
+       ,cl.endereco     as "Endereço"
+       ,cl.tipo_pessoa  as "Tipo de Pessoa"
+    from cliente cl inner join cidade c on (c.id = cl.id_cidade); 
+    
+ create view vw_usario as 
+ select id    as "Cód."
+ 	   ,nome  as "Nome"
+ 	   ,senha as "Senha"
+    from usuario;
+    
+ create view vw_material as 
+ select id		  	     as "Cód."
+       ,descricao        as "Decrição"
+       ,valor_unitario   as "Valor Unitario"
+    from material;
+    
+    
+create view vw_equipamento as 
+select id                  as "Cód."
+	  ,nome                as "Nome"
+	  ,marca               as "Marca"
+	  ,n_serie             as "Número de Série"
+  from equipamento;
+  
+ 
+ create view vw_tecnico as 
+ select id              as "Cód."
+       ,nome            as "Nome"
+       ,valor_hora	    as "Preço Hora"
+    from tecnico;
+    
+ 
+ create view vw_os           as 
+ select os.id                as "Cód."
+       ,os.data_entrada      as "Entrada"
+       ,os.descricao_servico as "Descrição do Serviço"
+       ,c.nome	             as "Cliente"
+       ,e.nome		         as "Equipamento"
+    from os  inner join cliente c on (c.id = os.id_cliente) 
+    inner join equipamento e  on (e.id = os.id_equipamento);
+    
+    select * from vw_os;
+ 
+
+ 
+ create view vw_pais as 
+ select id           as "Cód."
+ 	   ,nome         as "País"
+ 	   ,sigla        as "Sigla"
+ 	from pais;
+ 	
+ 	
+ create view vw_estado as 
+ select e.id           as   "Cód."
+       ,e.nome         as   "Estado"
+       ,e.uf           as   "UF"
+       ,p.nome         as   "Pais"
+    from estado e inner join pais p on (p.id = e.id_pais);
+    
+    
+   
